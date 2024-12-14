2d208c579033e80de27e42792c9f92278f7ab2f5
nishantnepal3 <chocowafers4@gmail.com>
2024-12-13 20:41:26 -0500

    added a new jest test file

--- test/sum.test.js
+++ test/sum.test.js
@@ -3,3 +3,10 @@ const sum = (a, b) => a + b;
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+const multiply = (a, b) => a * b;
+
+test('multiplies 2 * 2 equals 4', () => {
+  expect(multiply(2, 2)).toBe(4);
+});
+
